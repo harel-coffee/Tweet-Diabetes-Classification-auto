e70475f79ea3ee1ba0633341b9fc0095e8c877b9
Adahn <adrian-ahne1@web.de>
2018-11-09 14:58:56 +0100

    dd

--- db/personal_tweets_execute.sh
+++ db/personal_tweets_execute.sh
@@ -4,7 +4,7 @@
   --mode "local"
   --pathUserClassifier "/space/Work/spark/classifiers/best_model_user_classif_SVC_2018-08-16_15-47-50.sav"
   --pathTweetClassifier "/space/Work/spark/classifiers/best_model_tweets_classif_SVC_2018-08-16_15-47-50.sav"
-  --pathWordEmbedding "hdfs://bgdta1-demy:8020/data/twitter/track-analyse/FastText_model/ft_wordembeddings_08112018.model"
+  --pathWordEmbedding "/space/Work/spark/FastText_model/ft_wordembeddings_08112018.model"
   --pathData "hdfs://bgdta1-demy:8020/data/twitter/track-analyse/matching-tweets_diabetes_noRetweetsDuplicates.parquet"
   --scorePersonalMinimum 0.25
   --pathSave "/space/Work/spark/matching-tweets_diab_noRetweetsDupl_personal.parquet"
