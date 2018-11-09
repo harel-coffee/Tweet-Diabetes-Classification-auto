5ca31ec0bcd7ca01331388c068299adbda194c70
Adahn <adrian-ahne1@web.de>
2018-11-09 15:14:48 +0100

    fqsdf

--- db/personal_tweets.py
+++ db/personal_tweets.py
@@ -103,7 +103,7 @@ if __name__ == '__main__':
     wordEmbedding = FastText.load(args.pathWordEmbedding)
 
     print("Load data..")
-    tweets = readFile(args.pathData, engine="pyarrow")
+    tweets = readFile(args.pathData)
 
     print("Classify all tweets of an user and exclude all users with a mean score < {} ...".format(scorePersonalMinimum))
     tweets_user_pers = score_users(tweets, model_user_classif, wordEmbedding, score_personal_minimum=args.scorePersonalMinimum, textColumn=args.columnNameTextData)
