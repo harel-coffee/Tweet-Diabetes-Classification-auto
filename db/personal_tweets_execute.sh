2b3ed6026f360dc27c5e7dd94569f89308bcc3fb
Adahn <adrian-ahne1@web.de>
2018-11-09 15:09:19 +0100

    fqsdf

--- db/personal_tweets_execute.sh
+++ db/personal_tweets_execute.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-/space/hadoop/lib/python/bin/python3 personal_tweets.py
-  --mode "local"
-  --pathUserClassifier "/space/Work/spark/classifiers/best_model_user_classif_SVC_2018-08-14_15-33-39.sav"
-  --pathTweetClassifier "/space/Work/spark/classifiers/best_model_tweets_classif_SVC_2018-08-16_15-47-50.sav"
-  --pathWordEmbedding "/space/Work/spark/FastText_model/ft_wordembeddings_09112018.model"
-  --pathData "hdfs://bgdta1-demy:8020/data/twitter/track-analyse/matching-tweets_diabetes_noRetweetsDuplicates.parquet"
-  --scorePersonalMinimum 0.25
-  --pathSave "/space/Work/spark/matching-tweets_diab_noRetweetsDupl_personal.parquet"
-  --columnNameTextData "text"
+/space/hadoop/lib/python/bin/python3 personal_tweets.py \
+  --mode "local" \
+  --pathUserClassifier "/space/Work/spark/classifiers/best_model_user_classif_SVC_2018-08-14_15-33-39.sav" \
+  --pathTweetClassifier "/space/Work/spark/classifiers/best_model_tweets_classif_SVC_2018-08-16_15-47-50.sav" \
+  --pathWordEmbedding "/space/Work/spark/FastText_model/ft_wordembeddings_09112018.model" \
+  --pathData "hdfs://bgdta1-demy:8020/data/twitter/track-analyse/matching-tweets_diabetes_noRetweetsDuplicates.parquet" \
+  --scorePersonalMinimum 0.25 \
+  --pathSave "/space/Work/spark/matching-tweets_diab_noRetweetsDupl_personal.parquet" \
+  --columnNameTextData "text" 
